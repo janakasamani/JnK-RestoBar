0ae0af1b1b491656dd44a978bd27734d81144134
janakasamani <128301377+janakasamani@users.noreply.github.com>
2023-08-24 14:56:15 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+import express from "express";
+
+const app = express();
+const port = 8080;
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.render("index.ejs")
+  });
+
+app.get("/about", (req, res) => {
+    res.render("about.ejs")
+  });
+
+app.get("/menu", (req, res) => {
+    res.render("menu.ejs")
+  });
+  
+app.get("/contact", (req, res) => {
+    res.render("contact.ejs")
+  });
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
+  
